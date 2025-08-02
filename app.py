513c5f7c7554a968cb414b1616830a02af334749
Taras Shcherban <147646967+Miki535@users.noreply.github.com>
2025-08-02 14:16:56 +0300

    Update

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, json
 
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return jsonify({'message': 'Hello Api rabotaet!'})
+    with open("data.json", "r", encoding="utf-8") as json_file:
+        files = json.load(json_file)
+    return jsonify(files)
 
 if __name__ == '__main__':
     app.run(debug=True)
